c228654f75cfa3c2e6fdfe42de41768a117c590a
vadomerka <pradomiri@gmail.com>
2021-11-23 18:51:31 +0300

    pygame hello

--- first.py
+++ first.py
@@ -1,6 +1,26 @@
 import pygame
 
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, Pygame!", True, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    #  рамка
+    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
+
+    while pygame.event.wait().type != pygame.QUIT:
+        draw(screen)
+        pygame.display.flip()
+    pygame.quit()
