90e8f8907016647b57f1a575ca96b0d6e5aaae0d
vadomerka <pradomiri@gmail.com>
2021-11-26 12:24:53 +0300

    pygame moving hid_count

--- first.py
+++ first.py
@@ -8,37 +8,27 @@ def draw_cycle(sc, pos):
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 300, 300
+    size = width, height = 200, 200
     screen = pygame.display.set_mode(size)
     screen.fill(pygame.Color("black"))
-    repos = 0, 0
-    dif = 0, 0
-    pygame.draw.rect(screen, pygame.Color("green"), (*repos, 100, 100), width=0)
+    count = 0
     clock = pygame.time.Clock()
     fps = 60
     mouse_pos = None
     move = False
     running = True
     while running:
-        # screen.fill((0, 0, 0))
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             # при закрытии окна
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if repos[0] <= event.pos[0] <= repos[0] + 100 and \
-                        repos[1] <= event.pos[1] <= repos[1] + 100:
-                    mouse_pos = event.pos
-                    dif = mouse_pos[0] - repos[0], mouse_pos[1] - repos[1]
-            if event.type == pygame.MOUSEBUTTONUP:
-                mouse_pos = None
-            if event.type == pygame.MOUSEMOTION:
-                if mouse_pos:
-                    screen.fill(pygame.Color("black"))
-                    mouse_pos = event.pos
-                    repos = mouse_pos[0] - dif[0], mouse_pos[1] - dif[1]
-                    pygame.draw.rect(screen, pygame.Color("green"), (*repos, 100, 100), width=0)
-
+            if event.type == pygame.WINDOWHIDDEN:
+                count += 1
+                # print(count)
+        font = pygame.font.Font(None, 100)
+        text = font.render(str(count), True, (255, 0, 0))
+        screen.blit(text, (85, 60))
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
